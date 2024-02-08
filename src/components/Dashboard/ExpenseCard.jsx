e3d6e80ab73264151560cfcb0087915387e4c12c
Lucifer Morningstar <lucifer@morningstar.com>
2024-02-08 23:56:41 +0530

    minor changes

--- src/components/Dashboard/ExpenseCard.jsx
+++ src/components/Dashboard/ExpenseCard.jsx
@@ -1,26 +1,28 @@
-import styles from "../../styles/Dashboard/expenseCard.module.css"
-import receipt from "../../../public/img/receipt.png"
-import Image from "next/image"
+import styles from "../../styles/Dashboard/expenseCard.module.css";
+import receipt from "../../../public/img/receipt.png";
+import Image from "next/image";
 
-const ExpenseCard = ({ modal, setModal}) => {
-    return (
-        <div className={styles?.container} onClick={() => setModal({...modal, view: true})}>
-            <Image
-                className={styles?.image}
-                src={receipt}
-            />
+const ExpenseCard = ({ modal, setModal }) => {
+  return (
+    <div
+      className={styles?.container}
+      onClick={() => setModal({ ...modal, view: true })}
+    >
+      <Image className={styles?.image} src={receipt} />
 
-            <div className={styles?.verticalLine}></div>
+      <div className={styles?.verticalLine}></div>
 
-            <div className={styles?.rightContent}>
-                <p className={styles?.dateText}>12/01/2024</p>
-                <h4 className={styles?.expenseSubject}>Good Trip</h4>
-                <span className={styles?.expenseDesc}>Trip Description</span>
-                <div className={styles?.statusButton}>Aprooved</div>
-                <span className={styles?.reportedToText}>Reported To:- Vinesh Sharma (HR)</span>
-            </div>
-        </div>
-    )
-}
+      <div className={styles?.rightContent}>
+        <p className={styles?.dateText}>12/01/2024</p>
+        <h4 className={styles?.expenseSubject}>Good Trip</h4>
+        <span className={styles?.expenseDesc}>Trip Description</span>
+        <div className={styles?.statusButton}>Aprooved</div>
+        <span className={styles?.reportedToText}>
+          Reported To:- Vinesh Sharma (HR)
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default ExpenseCard
+export default ExpenseCard;
