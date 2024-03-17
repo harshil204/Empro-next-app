03b964b7a67bf62daadf6f81b0c134c65f3cab1d
Lucifer Morningstar <lucifer@morningstar.com>
2024-03-17 23:30:40 +0530

    sunday stuff

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,49 +1,51 @@
-import axios from 'axios'
-import NextAuth from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
+import axios from "axios";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import jwt from "jsonwebtoken";
 
 const handler = NextAuth({
-    providers: [
-        CredentialsProvider({
-            name: "credentials",
-            async authorize(credentials, req) {
-                try {
-                    const res = await axios.post(`http://localhost:5000/api/user/login`, {
-                        "email": credentials?.email,
-                        "password": credentials?.password
-                    })
-                    console.log("response ==> ", res?.data)
-                    const user = res?.data?.user
-                    return true
-                    // if (user) {
-                    //     return user
-                    // } else {
-                    //     return null
-                    // }
-                } catch (error) {
-                    console.log("error caught")
-                }
-               
-            }
-        })
-    ],
-    callbacks: {
-        async jwt({ token, user }) {
-            console.log("user console from jwt Callback --> ", {user, token})
-            if (user) {
-              token.username = user.username;
-              token.profileImage = user.profileImage;
-              token.role = user.role;
-              token.id = user.id;
-            }
-            return token;
-          },
-        async session({ session, user, token }) {
-            console.log("session called ==> ", {session, user, token})
-            return session
-          },
-    }
-})
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      async authorize(credentials, req) {
+        try {
+          const res = await axios.post(`http://localhost:5000/api/user/login`, {
+            email: credentials?.email,
+            password: credentials?.password,
+          });
 
+          const verified = await jwt.verify(
+            res?.data?.token,
+            process.env.JWT_SECRET
+          );
 
-export { handler as GET, handler as POST }
+          return (
+            { user: verified?.user, token: res?.data?.token } ??
+            console.log("Something went wrong")
+          );
+        } catch (error) {
+          console.log("error caught");
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user?.token) {
+        token.jwt = user.token;
+      }
+
+      return token;
+    },
+    async session({ session, user, token }) {
+      session.accessToken = token.jwt;
+      user = jwt.verify(token.jwt, process?.env?.JWT_SECRET);
+      delete user?.exp;
+      delete user?.iat;
+      session.user = { ...user };
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
